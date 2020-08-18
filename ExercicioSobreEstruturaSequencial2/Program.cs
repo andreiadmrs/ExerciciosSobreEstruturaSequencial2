afff220cfebb39f133637af1d8fee02ded13e7a5
Andreia de Moraes <andreiadmrs@gmail.com>
2020-08-17 22:18:40 -0300

    exercicio do curso da udemy csharp

---
+++ ExercicioSobreEstruturaSequencial2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioSobreEstruturaSequencial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.*/
+
+            double pi = 3.14159;
+
+            Console.WriteLine("Digite o raio do círculo: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = pi * raio * raio;
+
+            Console.WriteLine($"O valor total da área do círculo é " + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
